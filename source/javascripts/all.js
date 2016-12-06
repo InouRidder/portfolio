aad07316f91306a9bb0d115b787179ee051b43ef
Inou Ridder <inouridder@gmail.com>
2016-12-06 17:57:24 +0100

    Add Dropdown Menu, About me card, Initial tab coding

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -31,3 +31,34 @@ $(document).ready(function(){
     });
   });
 
+
+$(function(){
+
+ $(".tab").on("click", function(e){
+   // Change active tab
+
+
+   // remove active class...
+   $(".tab").removeClass("active")
+
+   // put active class on this..
+   $(this).addClass("active")
+
+   // Hide all tab-content (use class="hidden")
+   $(".tab-content").addClass("hidden")
+   var target = $(this).data("target");
+   $(target).removeClass("hidden");
+   // $(this)
+   // Show target tab-content (use class="hidden")
+ });
+
+});
+
+// $(document).ready(function(){
+//   var obj = this;
+//   obj.dd.on('click', function(event){
+//     $('.active').not($(this)).removeClass('active');
+//     $(this).toggleClass('active');
+//     event.stopPropagation();
+//   });
+// };
