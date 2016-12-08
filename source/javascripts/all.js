d8de04a587c87f9761d6b4a5543b5e0e03959075
Inou Ridder <inouridder@gmail.com>
2016-12-08 11:43:31 +0100

    Add About Block, Start on Cards

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -32,27 +32,22 @@ $(document).ready(function(){
   });
 
 
+
 $(function(){
 
- $(".tab").on("click", function(e){
-   // Change active tab
-
-
-   // remove active class...
-   $(".tab").removeClass("active")
-
-   // put active class on this..
-   $(this).addClass("active")
-
-   // Hide all tab-content (use class="hidden")
-   $(".tab-content").addClass("hidden")
-   var target = $(this).data("target");
-   $(target).removeClass("hidden");
-   // $(this)
-   // Show target tab-content (use class="hidden")
- });
-
+  $(".tab").on("click", function(e){
+    e.preventDefault();
+    var thisTarget = $(this).data("target")
+    // Change active tab
+    $('.tab').removeClass('active');
+    $(this).addClass('active');
+    // Hide all tab-content (use class="hidden")
+    $('.tab-content').addClass('hidden');
+    // Show target tab-content (use class="hidden")
+    $(thisTarget).removeClass('hidden');
+  });
 });
+
 
 // $(document).ready(function(){
 //   var obj = this;
